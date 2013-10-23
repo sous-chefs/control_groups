2d6779362c9365361b85f3d6fcd33c336170719f
Chris Roberts <chrisroberts.code@gmail.com>
2013-10-23 13:16:04 -0700

    Update version for development 0.1.5

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer_email "support@hw-ops.com"
 license          "Apache 2.0"
 description      "Provides and configures cgroups"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.4"
+version          "0.1.5"
 
 supports 'ubuntu'

b1d9f9a67645a45e95ca4406eabf20b22ae99b7d
Gary Bright <digitalgaz@hotmail.com>
2019-06-21 14:10:25 +0100

    Linting Fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
-
 default[:control_groups][:mounts] = {
-  :cpu => '/sys/fs/cgroup/cpu',
-  :cpuacct => '/sys/fs/cgroup/cpuacct',
-  :cpuset => '/sys/fs/cgroup/cpuset',
-  :devices => '/sys/fs/cgroup/devices',
-  :memory => '/sys/fs/cgroup/memory',
-  :freezer => '/sys/fs/cgroup/freezer'
+  cpu: '/sys/fs/cgroup/cpu',
+  cpuacct: '/sys/fs/cgroup/cpuacct',
+  cpuset: '/sys/fs/cgroup/cpuset',
+  devices: '/sys/fs/cgroup/devices',
+  memory: '/sys/fs/cgroup/memory',
+  freezer: '/sys/fs/cgroup/freezer',
+}

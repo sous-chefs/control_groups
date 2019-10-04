5e92a1eb53ba43d279cc1ef28f284f3acfec60f4
Mark E. Schill <Mark.Schill@cmschill.net>
2019-10-03 21:07:45 -0400

    Style fixes
    Signed-off-by: Mark E. Schill <Mark.Schill@cmschill.net>

--- metadata.rb
+++ metadata.rb
@@ -3,11 +3,11 @@ maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 license          'Apache-2.0'
 description      'Provides and configures cgroups'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+
 version          '0.1.6'
 source_url       'https://github.com/sous-chefs/swap'
 issues_url       'https://github.com/sous-chefs/swap/issues'
-chef_version     '>= 13' if respond_to?(:chef_version)
+chef_version     '>= 13'
 
 %w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon zlinux).each do |os|
   supports os
